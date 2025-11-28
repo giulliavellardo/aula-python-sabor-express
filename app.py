3536e59f121761b747f098d9871e05d6b7273a63
giullia vellardo <giulliavellardo@gmail.com>
2025-11-28 01:07:12 -0300

    Atualização no app.py

--- app.py
+++ app.py
@@ -8,11 +8,22 @@ print ("""
 
 
 """)
-
 print ('1. cadastrar restaurante')
 print ('2. listar restaurante')
 print ('3. ativar restaurante')
 print ('4. sair\n')
 
 opcao_escolhida = input ('Escolha uma opção: ')
-print ('você escolheu a opção', opcao_escolhida)
+
+if opcao_escolhida == 1:
+    print ('cadastrar restaurantes')
+elif opcao_escolhida == 2:
+    print ('listar restaurantes')
+elif opcao_escolhida == 3:
+    print ('ativar restaurante')
+else opcao_escolhida == 4:
+print ('finalizar programa')
+
+
+
+
